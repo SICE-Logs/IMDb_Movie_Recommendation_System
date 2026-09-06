9ccf75b7bf8efa3b901f507f22cfa7d6e5f9e8e1
SICE-Logs <logajitselvakumar@gmail.com>
2026-09-06 16:30:01 +0530

    Create Files

---
+++ src/config.py
@@ -0,0 +1,13 @@
+from pathlib import Path
+
+
+# Project root directory
+PROJECT_ROOT = Path(__file__).resolve().parent.parent
+
+# Project directories
+DATA_DIR = PROJECT_ROOT / "data"
+MODELS_DIR = PROJECT_ROOT / "models"
+NOTEBOOKS_DIR = PROJECT_ROOT / "notebooks"
+
+# Dataset
+RAW_DATA_PATH = DATA_DIR / "IMDBRecSys.csv"
